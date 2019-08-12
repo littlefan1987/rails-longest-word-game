cf456b7eaaed6886f6d3bf69bd2a8765f27947a4
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-12 14:39:20 +0200

    word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,39 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a.sample }
+    return @letters
+  end
+
+  def score
+    @grid = params[:grid]
+    @answer = params[:word]
+    # pass each letter as string
+    grid_letters = @grid.each_char { |letter| print letter, '' }
+    if !letter_in_grid
+      @result = "Sorry, #{@answer.upcase} can’t be built out of #{grid_letters}."
+    elsif !english_word(@answer)
+      @result = "Sorry, #{@answer.upcase} does not seem to be an English word."
+    elsif letter_in_grid && !english_word(@answer)
+      @result = "Sorry but #{@answer.upcase} does not seem to be an English word."
+    else letter_in_grid && !english_word(@answer)
+      @result = "Congratulation! #{@answer.upcase} is a valid English word."
+    end
+  end
+
+  private
+
+  def letter_in_grid
+    @answer.upcase.chars.all? { |letter| @grid.include?(letter) }
+  end
+
+  def english_word(keyword)
+    url = "https://wagon-dictionary.herokuapp.com/#{keyword}"
+    word_dictionary = open(url).read
+    word = JSON.parse(word_dictionary)
+    return word['found']
+  end
+end
